29fb085706bb21548bb05feb587dbb9786d6da09
billy.wang <billy.wangbb@gmail.com>
2016-04-14 18:37:24 +0800

    add flybirdBlePeripheral support

---
+++ Projects/ble/FlybirdBLEPeripheral/Source/defined.h
@@ -0,0 +1,16 @@
+#ifndef DEFINE_H
+#define DEFINE_H
+
+#define INT_HEAP_LEN 3072
+#define HALNODEBUG
+#define OSAL_CBTIMER_NUM_TASKS 1
+#define HAL_AES_DMA TRUE
+#define HAL_DMA TRUE
+#define POWER_SAVING
+#define xPLUS_BROADCASTER
+#define HAL_LCD TRUE
+#define HAL_LED TRUE
+#define HAL_KEY TRUE
+//#define HAL_UART TRUE
+
+#endif
